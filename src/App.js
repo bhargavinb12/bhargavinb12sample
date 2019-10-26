e88441dd628ded668b39dfb2708fc19074093b3c
bhargavinb12 <35928626+bhargavinb12@users.noreply.github.com>
2019-10-26 16:03:04 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import logo from "./logo.svg";
+import Home from "./components/Home";
+import Grid from "./components/Grid";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import About from "./components/About";
+
+import ButtonAppBar from "./components/ButtonAppBar";
+import "./App.css";
+
+const App = props => {
+  return (
+    <div className="App">
+      <Grid />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import { Switch, Route } from "react-router-dom";
+import { render } from "react-dom";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import About from "./components/About";
+import ButtonAppBar from "./components/ButtonAppBar";
+
+// import Grid from "./components/About";
+
+const routing = (
+  <Router>
+    <div>
+      <ButtonAppBar />
+      <Route exact path="/" component={App} />
+      <Route path="/about" component={About} />
+
+      <Route path="/resume" component={Resume} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+);
+ReactDOM.render(routing, document.getElementById("root"));
